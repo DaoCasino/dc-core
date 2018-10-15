1805f13144e881fec9a6fe296a74a3e9378f36fd
Michael Oznobkin <michaeloznobkin@Michaels-MacBook-Pro-2.local>
2018-10-15 20:45:17 +0300

    chore: add blockchainNetwork to dapp shared room

--- src/interfaces/IDapp.ts
+++ src/interfaces/IDapp.ts
@@ -3,11 +3,13 @@ import { Eth } from "dc-ethereum-utils"
 import { ContractInfo } from "dc-configs"
 import { GameLogicFunction, DAppInstanceView } from "./index"
 import { platform } from "os"
+import { blockchainNetworkConfigs } from "dc-configs/lib/blockchainNetworks"
 
 export interface DAppParams {
   slug: string
   rules: any
   platformId: string
+  blockchainNetwork: string
   gameLogicFunction: GameLogicFunction
   contract: ContractInfo
   roomProvider: IMessagingProvider
