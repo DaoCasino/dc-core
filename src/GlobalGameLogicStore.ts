22e05ccdba2157f29f183b2d5a4f55f560b25586
moznobkin <oznobkin@gmail.com>
2018-10-23 16:58:27 +0300

    chore: infura

--- src/GlobalGameLogicStore.ts
+++ src/GlobalGameLogicStore.ts
@@ -1,9 +1,4 @@
 export class GlobalGameLogicStore {
-  /**
-   * Define DApp logic constructor function
-   * @param {string} dapp_slug         unique slug of your dapp
-   * @param {function} logic_constructor constructor Dapp logic
-   */
   getGameLogic(slug) {
     const globalStore: any = global || window
     return globalStore.DAppsLogic[slug]
