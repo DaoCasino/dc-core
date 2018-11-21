f6e97d0045a248326944553afeea7d3073158ebd
alexstep <alex.step.kellas@gmail.com>
2018-11-21 17:13:35 +0300

    refactor(dc-core): add test for random generator

---
+++ src/__tests__/generateRND.ts
@@ -0,0 +1,41 @@
+import { config } from "dc-configs"
+import { Logger } from "dc-logging"
+import { Eth, makeSeed } from "dc-ethereum-utils"
+import { generateRandom } from "../Rnd"
+
+const log = new Logger("generateRND:Test:")
+const {
+  gasPrice: price,
+  gasLimit: limit,
+  web3HttpProviderUrl: httpProviderUrl,
+  contracts,
+  walletName,
+  privateKey
+} = config.default
+
+const generateRNDTEST = async (rangeStart, rangeEnd) => {
+  const eth = new Eth({
+    walletName,
+    httpProviderUrl,
+    ERC20ContractInfo: contracts.ERC20,
+    gasParams: { price, limit }
+  })
+  eth.initAccount(privateKey)
+  const seed = makeSeed()
+  const hash = eth.signData([{ t: "bytes32", v: seed }])
+
+  const rnd = generateRandom(
+    [
+      [rangeStart, rangeEnd],
+      [rangeStart * 2, rangeEnd * 3],
+      [rangeStart, rangeEnd * 5]
+    ],
+    hash
+  )
+
+  log.debug(rnd)
+}
+
+for (let i = 0; i < 10; i++) {
+  generateRNDTEST(i, i * 2)
+}
