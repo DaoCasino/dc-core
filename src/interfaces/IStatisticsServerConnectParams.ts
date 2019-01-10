9abdd81fe9cede9a9790f492aa13d69ca5e7f5a6
dl <dl@dao.casino>
2019-01-10 19:34:44 +0300

    fix(stat): bugfix

--- src/interfaces/IStatisticsServerConnectParams.ts
+++ src/interfaces/IStatisticsServerConnectParams.ts
@@ -1,7 +1,5 @@
-import { ProtocolType } from '@daocasino/dc-statistics-client'
-
 export interface IStatisticsServerConnectParams {
   authKey: string,
   host: string,
-  protocol: ProtocolType
+  protocol: string
 }
